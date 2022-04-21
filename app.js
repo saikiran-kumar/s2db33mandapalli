1b811310ff481ed0c96fd3093150e2fc8201a306
saikiran-kumar <saikirankumar34@gmail.com>
2022-04-21 12:40:22 -0500

    added with passport module

--- app.js
+++ app.js
@@ -2,10 +2,24 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 var grocery = require("./models/grocery");
-
-
+var logger = require('morgan');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+  Account.findOne({ username: username }, function (err, user) {
+  if (err) { return done(err); }
+  if (!user) {
+  return done(null, false, { message: 'Incorrect username.' });
+  }
+  if (!user.validPassword(password)) {
+  return done(null, false, { message: 'Incorrect password.' });
+  }
+  return done(null, user);
+  });
+}))
+  
 const connectionString =process.env.MONGO_CON;
 mongoose = require('mongoose');
 mongoose.connect(connectionString,{useNewUrlParser: true,useUnifiedTopology: true});
@@ -27,6 +41,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+ }));
+ app.use(passport.initialize());
+ app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -65,6 +86,13 @@ async function recreateDB(){
   console.log("Fourth object saved")
   });
  }
+ // passport config
+// Use the existing connection
+// The Account model
+var Account =require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
  let reseed = true;
  if (reseed) { recreateDB();}
 // catch 404 and forward to error handler
